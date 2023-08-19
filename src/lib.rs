64406b1b081881ad8aeae9008440cdbb6a65987d
Crispin Koech <crispinkoech.ck@gmail.com>
2023-08-19 21:34:39 +0300

    Add ::build() function to structs with named fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use proc_macro::TokenStream;
-use proc_macro2::Span;
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
 use syn::{
-    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Generics, Ident,
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, GenericArgument,
+    Generics, Ident, PathArguments, Type,
 };
 
 #[proc_macro_derive(Builder)]
@@ -24,6 +25,7 @@ pub fn derive_builder(annotated_item: TokenStream) -> TokenStream {
 fn struct_builder(body: DataStruct, generics: Generics, struct_identifier: Ident) -> TokenStream {
     let builder_identifier = builder_identifier(&struct_identifier);
 
+    // Attach builder function to annotated item
     let mut builder: TokenStream = quote! {
         impl #generics #struct_identifier #generics {
             pub fn builder() -> #builder_identifier #generics {
@@ -33,9 +35,10 @@ fn struct_builder(body: DataStruct, generics: Generics, struct_identifier: Ident
     }
     .into();
 
+    // Attach a builder struct fields and implement
     let builder_implementation = match body.fields {
         Fields::Named(fields) => {
-            struct_with_named_fields_builder(fields, generics, struct_identifier)
+            struct_with_named_fields_tokens(fields, generics, struct_identifier)
         }
         Fields::Unnamed(_) => unimplemented!(),
         Fields::Unit => unimplemented!(),
@@ -45,50 +48,44 @@ fn struct_builder(body: DataStruct, generics: Generics, struct_identifier: Ident
     builder
 }
 
-fn struct_with_named_fields_builder(
+fn struct_with_named_fields_tokens(
     fields: FieldsNamed,
     generics: Generics,
     struct_identifier: Ident,
 ) -> TokenStream {
     let builder_identifier = builder_identifier(&struct_identifier);
 
-    let mut types = vec![];
-    let mut initial_fields = vec![];
+    let mut declarations = vec![];
+    let mut initializers = vec![];
     let mut setters = vec![];
+    let mut getters = vec![];
 
     for field in fields.named.iter() {
-        let Field {
-            ident: field_name,
-            ty: field_type,
-            ..
-        } = field;
-
-        types.push(quote! {
-            #field_name: Option<#field_type>,
-        });
-
-        initial_fields.push(quote! {
-            #field_name: None,
-        });
-
-        setters.push(quote! {
-            pub fn #field_name(mut self, #field_name: #field_type) -> Self {
-                self.#field_name = Some(#field_name);
-                self
-            }
-        })
+        let (declare, initialize, set, get) = named_fields_tokens(field);
+        declarations.push(declare);
+        initializers.push(initialize);
+        setters.push(set);
+        getters.push(get)
     }
 
     quote! {
         struct #builder_identifier #generics {
-            #(#types)*
+            #(#declarations)*
         }
 
         impl #generics #builder_identifier #generics {
-            pub fn new () -> Self  {
+            pub fn new() -> Self  {
                 #builder_identifier {
-                    #(#initial_fields)*
+                    #(#initializers)*
                 }
+            }
+
+            pub fn build(&self) -> Result<#struct_identifier #generics, Box<dyn std::error::Error>> {
+                let result = #struct_identifier {
+                    #(#getters)*
+                };
+
+                Ok(result)
             }
 
             #(#setters)*
@@ -97,6 +94,91 @@ fn struct_with_named_fields_builder(
     .into()
 }
 
+fn named_fields_tokens(field: &Field) -> (TokenStream2, TokenStream2, TokenStream2, TokenStream2) {
+    let Field {
+        ident: field_name,
+        ty: field_type,
+        ..
+    } = field;
+    match match_field_type(field_type) {
+        MatchedFieldType::Option(t) => (
+            quote! {
+                #field_name: #field_type,
+            },
+            quote! {
+                #field_name: None,
+            },
+            quote! {
+                pub fn #field_name(mut self, #field_name: #t) -> Self {
+                    self.#field_name = Some(#field_name);
+                    self
+                }
+            },
+            quote! {
+                #field_name: self.#field_name.to_owned(),
+            },
+        ),
+        MatchedFieldType::Vec(t) => (
+            quote! {
+                #field_name: #field_type,
+            },
+            quote! {
+                #field_name: vec![],
+            },
+            quote! {
+                pub fn #field_name(mut self, #field_name: #t) -> Self {
+                    self.#field_name.push(#field_name);
+                    self
+                }
+            },
+            quote! {
+                #field_name: self.#field_name.to_owned(),
+            },
+        ),
+        MatchedFieldType::Other => (
+            quote! {
+                #field_name: Option<#field_type>,
+            },
+            quote! {
+                #field_name: None,
+            },
+            quote! {
+                pub fn #field_name(mut self, #field_name: #field_type) -> Self {
+                    self.#field_name = Some(#field_name);
+                    self
+                }
+            },
+            quote! {
+                #field_name: self.#field_name.to_owned().ok_or("Field #field_name unset")?,
+            },
+        ),
+    }
+}
+
 fn builder_identifier(identifier: &Ident) -> Ident {
     Ident::new(&format!("{}Builder", identifier), Span::call_site())
+}
+
+// TODO: This is too messy, find a better approach to parse declarations.
+fn match_field_type(field_type: &Type) -> MatchedFieldType {
+    let path_segments = match field_type {
+        Type::Path(path) if path.qself.is_none() => &path.path.segments,
+        _ => return MatchedFieldType::Other,
+    };
+    let last_segment = path_segments.iter().last().unwrap();
+    let segment_arguments = match &last_segment.arguments {
+        PathArguments::AngleBracketed(params) => params.args.first().unwrap(),
+        _ => return MatchedFieldType::Other,
+    };
+    match segment_arguments {
+        GenericArgument::Type(t) if last_segment.ident == "Option" => MatchedFieldType::Option(t),
+        GenericArgument::Type(t) if last_segment.ident == "Vec" => MatchedFieldType::Vec(t),
+        _ => MatchedFieldType::Other,
+    }
+}
+
+enum MatchedFieldType<'t> {
+    Vec(&'t Type),
+    Option(&'t Type),
+    Other,
 }
--- tests/test_struct_with_named_fields.rs
+++ tests/test_struct_with_named_fields.rs
@@ -4,15 +4,48 @@ use derive_builder::Builder;
 struct TestStruct<'t> {
     field_a: i8,
     field_b: String,
-    field_c: Option<&'t str>,
+    field_c: &'t str,
+    field_d: Vec<i8>,
+    field_e: Option<String>,
+    field_f: Option<&'t str>,
 }
 
 fn main() {
+    test_fully_filled_struct();
+    test_partially_filled_struct();
+}
+
+fn test_fully_filled_struct() {
     let test_case = TestStruct::builder()
         .field_a(1)
-        .field_b("example".to_string());
+        .field_b("example".to_string())
+        .field_c("string slice")
+        .field_d(10)
+        .field_e("another string slice".to_string())
+        .field_f("yet another string slice")
+        .build()
+        .unwrap();
 
-    assert_eq!(test_case.field_a, Some(1));
-    assert_eq!(test_case.field_b, Some("example".to_string()));
-    assert_eq!(test_case.field_c, None);
+    assert_eq!(test_case.field_a, 1);
+    assert_eq!(test_case.field_b, "example".to_string());
+    assert_eq!(test_case.field_c, "string slice");
+    assert_eq!(test_case.field_d, vec![10]);
+    assert_eq!(test_case.field_e, Some("another string slice".to_string()));
+    assert_eq!(test_case.field_f, Some("yet another string slice"));
+}
+
+fn test_partially_filled_struct() {
+    let test_case = TestStruct::builder()
+        .field_a(1)
+        .field_b("example".to_string())
+        .field_c("string slice")
+        .build()
+        .unwrap();
+
+    assert_eq!(test_case.field_a, 1);
+    assert_eq!(test_case.field_b, "example".to_string());
+    assert_eq!(test_case.field_c, "string slice");
+    assert_eq!(test_case.field_d, vec![]);
+    assert_eq!(test_case.field_e, None);
+    assert_eq!(test_case.field_f, None);
 }
